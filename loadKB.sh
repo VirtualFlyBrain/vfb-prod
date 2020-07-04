6b1f040b1189e0a68c4d37eb47f6cd998e253b64
Rob Court <rcourt@ed.ac.uk>
2020-07-04 21:40:31 +0100

    using optional backup file restore

--- loadKB.sh
+++ loadKB.sh
@@ -6,21 +6,23 @@ echo 'Allow new neo4j2owl plugin to make changes..'
 echo 'dbms.security.procedures.unrestricted=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
 #echo 'dbms.security.procedures.whitelist=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
 
-if [ ! -d /data/databases/graph.db ]; then
-  if [ ! -d /backup/KBW-RESTORE.db ]; then
-    echo 'Resore KB from archive backup'
-    cd /opt/VFB/backup/
-    rm /opt/VFB/backup/VFB-KB2.tar.gz
-    wget http://data.virtualflybrain.org/archive/VFB-KB2.tar.gz 
-    tar -xzvf VFB-KB2.tar.gz
-    mkdir -p /backup/KBW-RESTORE.db/
-    find /opt/VFB/backup/ -name 'KBW-RESTORE.db' -exec cp -vr "{}" /backup/ +
-    rm -rf /opt/VFB/backup/*
-    cd -
-  fi
-  if [ -d /backup/KBW-RESTORE.db ]; then
-    echo 'Resore KB from given backup'
-    /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --force=true
+if [ -n "${BACKUPFILE}" ]; then
+  if [ ! -d /data/databases/graph.db ]; then
+    if [ ! -d /backup/DB-RESTORE.db ]; then
+      echo 'Resore KB from archive backup'
+      cd /opt/VFB/backup/
+      rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
+      wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
+      tar -xzvf ${BACKUPFILE}.tar.gz
+      mkdir -p /backup/DB-RESTORE.db/
+      find /opt/VFB/backup/ -name 'DB-RESTORE.db' -exec cp -vr "{}" /backup/ +
+      rm -rf /opt/VFB/backup/*
+      cd -
+    fi
+    if [ -d /backup/DB-RESTORE.db ]; then
+      echo 'Resore KB from given backup'
+      /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/DB-RESTORE.db --force=true
+    fi
   fi
 fi
 
