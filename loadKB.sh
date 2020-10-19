efa27846432d32b18d2447c88542224954d2f7cd
Rob Court <rcourt@ed.ac.uk>
2020-10-19 10:50:58 +0100

    forcing load from local backup

--- loadKB.sh
+++ loadKB.sh
@@ -5,25 +5,9 @@ sed -i s/read_only=.*/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 echo 'Allow new neo4j2owl plugin to make changes..'
 echo 'dbms.security.procedures.unrestricted=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
 #echo 'dbms.security.procedures.whitelist=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
-
-if [ -n "${BACKUPFILE}" ]; then
-  if [ ! -d /data/databases/graph.db ]; then
-    if [ ! -d /backup/DB-RESTORE.db ]; then
-      echo 'Resore KB from archive backup'
-      cd /opt/VFB/backup/
-      rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
-      wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
-      tar -xzvf ${BACKUPFILE}.tar.gz
-      mkdir -p /backup/DB-RESTORE.db/
-      find /opt/VFB/backup/ -name 'DB-RESTORE.db' -exec cp -vr "{}" /backup/ +
-      rm -rf /opt/VFB/backup/*
-      cd -
-    fi
-    if [ -d /backup/DB-RESTORE.db ]; then
-      echo 'Resore KB from given backup'
-      /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/DB-RESTORE.db --force=true
-    fi
-  fi
+if [ -d /backup/DB-RESTORE.db ]; then
+  echo 'Resore KB from given backup'
+  /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/DB-RESTORE.db --force=true
 fi
 
 echo -e '\nSTARTING VFB KB SERVER\n' >> /var/lib/neo4j/logs/query.log
