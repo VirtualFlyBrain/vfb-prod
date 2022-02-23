812cca30afd40f10063282552e6cea033a41df60
hkir-dev <huseyin_kir@yahoo.com>
2022-02-23 11:02:05 +0000

    disabled loading kb

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,13 @@ ENV BACKUPFILE="VFB-PDB2-v4"
 
 ENV NEO4J_dbms_memory_heap_maxSize=4G
 ENV NEO4J_dbms_memory_heap_initial__size=1G
-ENV NEO4J_dbms_read__only=true
+ENV NEO4J_dbms_read__only=false
 ENV NEO4J_dbms_security_procedures_unrestricted=ebi.spot.neo4j2owl.*,apoc.*,gds.*
+ENV NEO4J_dbms_directories_import=import
+ENV NEO4J_dbms_security_allow_csv_import_from_file_urls=true
+# Log4J CVE-2021-44228 vulnerability Mitigation for Neo4j
+ENV NEO4J_dbms_jvm_additional="-Dlog4j2.formatMsgNoLookups=true -Dlog4j2.disable.jmx=true"
+ENV LOG4J_FORMAT_MSG_NO_LOOKUPS=true
 
 RUN apt-get -y update && apt-get -y install tar gzip curl wget zip unzip
 
@@ -25,9 +30,9 @@ ARG GDS_JAR=https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/gr
 ENV GDS_JAR ${GDS_JAR}
 RUN wget $GDS_JAR -O /var/lib/neo4j/plugins/gds.zip && cd /var/lib/neo4j/plugins/ && unzip gds.zip && rm gds.zip
 
-RUN mkdir -p /opt/VFB/backup/ 
+RUN mkdir -p /opt/VFB/backup/
 
-RUN chmod -R 777 /opt/VFB/backup/ 
+RUN chmod -R 777 /opt/VFB/backup/
 
 RUN chmod +x /opt/VFB/loadKB.sh
 
--- loadKB.sh
+++ loadKB.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=.*/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 
@@ -6,32 +6,12 @@ echo 'Allow new neo4j2owl plugin to make changes..'
 echo 'dbms.security.procedures.unrestricted=ebi.spot.neo4j2owl.*,apoc.*,gds.*' >> ${NEOSERCONF}
 #echo 'dbms.security.procedures.whitelist=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
 
-if [ -n "${BACKUPFILE}" ]; then
-  if [ ! -d /data/databases/neo4j ]; then
-    echo 'Resore KB from archive backup'
-    cd /opt/VFB/backup/
-    rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
-    wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
-    tar -xzvf ${BACKUPFILE}.tar.gz
-    mkdir -p /var/lib/neo4j/data/databases/
-    bkdir=$(ls /opt/VFB/backup/*/neostore)
-    mv -v "${bkdir/\/neostore/}" /opt/VFB/backup/neo4j
-    neo4j-admin restore --from /opt/VFB/backup/neo4j --force
-    rm -rf /opt/VFB/backup/*
-    cd -
-  fi
-fi
-
 echo -e '\nSTARTING VFB KB SERVER\n' >> /var/lib/neo4j/logs/query.log
 
-chown -R neo4j /logs || :
-chown -R neo4j /data || :
-chown -R neo4j /import || :
-chown -R neo4j /var/lib/neo4j/plugins || :
-chmod -R 777 /logs || :
-chmod -R 777 /data || :
-chmod -R 777 /import || :
-chmod -R 777 /var/lib/neo4j/plugins || :
+if [ -z "$(ls -A /var/lib/neo4j/import)" ]; then
+  echo "Transfering CSVs to import"
+  cp -R $CSV_IMPORTS/. /var/lib/neo4j/import
+fi
 
 #Output the query log to docker log:
 tail -f /var/lib/neo4j/logs/query.log >/proc/1/fd/1 &
