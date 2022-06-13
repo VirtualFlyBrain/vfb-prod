3c486fef169cb389dd3e45f28a3fb934e80e2b42
Rob Court <rcourt@ed.ac.uk>
2022-06-13 07:21:38 +0100

    Update loadKB.sh

--- loadKB.sh
+++ loadKB.sh
@@ -1,32 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=.*/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 
 echo 'Allow new neo4j2owl plugin to make changes..'
-echo 'dbms.security.procedures.unrestricted=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
-#echo 'dbms.security.procedures.whitelist=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
+echo 'dbms.security.procedures.unrestricted=ebi.spot.neo4j2owl.*,apoc.*,gds.*' >> ${NEOSERCONF}
 
 if [ -n "${BACKUPFILE}" ]; then
-  if [ ! -d /data/databases/graph.db ]; then
-    if [ ! -d /backup/DB-RESTORE.db ]; then
-      echo 'Resore KB from archive backup'
-      cd /opt/VFB/backup/
-      rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
-      wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
-      tar -xzvf ${BACKUPFILE}.tar.gz
-      mkdir -p /backup/DB-RESTORE.db/
-      find /opt/VFB/backup/ -name 'DB-RESTORE.db' -exec cp -vr "{}" /backup/ +
-      rm -rf /opt/VFB/backup/*
-      cd -
-    fi
-    if [ -d /backup/DB-RESTORE.db ]; then
-      echo 'Resore KB from given backup'
-      /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/DB-RESTORE.db --force=true
-    fi
+  if [ ! -d /data/databases/neo4j ]; then
+    echo 'Resore KB from archive backup'
+    cd /opt/VFB/backup/
+    rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
+    wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
+    tar -xzvf ${BACKUPFILE}.tar.gz
+    mkdir -p /var/lib/neo4j/data/databases/
+    bkdir=$(ls /opt/VFB/backup/*/neostore)
+    mv -v "${bkdir/\/neostore/}" /opt/VFB/backup/neo4j
+    neo4j-admin restore --from /opt/VFB/backup/neo4j --force
+    rm -rf /opt/VFB/backup/*
+    cd -
   fi
 fi
 
 echo -e '\nSTARTING VFB KB SERVER\n' >> /var/lib/neo4j/logs/query.log
+
+chown -R neo4j /logs || :
+chown -R neo4j /data || :
+chown -R neo4j /import || :
+chown -R neo4j /var/lib/neo4j/plugins || :
+chmod -R 777 /logs || :
+chmod -R 777 /data || :
+chmod -R 777 /import || :
+chmod -R 777 /var/lib/neo4j/plugins || :
 
 #Output the query log to docker log:
 tail -f /var/lib/neo4j/logs/query.log >/proc/1/fd/1 &
